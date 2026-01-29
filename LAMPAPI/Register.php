5c082b7ea19516f03d0fd90f7a53e9e5362e0459
William <williamgil1160@gmail.com>
2026-01-29 17:17:08 +0000

    Fixed error in Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -4,8 +4,8 @@
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
-    $login = "";
-    $password = "";
+        $login = "";
+        $password = "";
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
@@ -20,28 +20,29 @@
 		$stmt1->execute();
 		$result = $stmt1->get_result();
 
-        // if user already exists, reject them
+                // if user already exists, reject them
 		if( $row = $result->fetch_assoc()  )
 		{
-            returnWithError("Username Taken");
-        }
+                      returnWithError("Username Taken");
+                }
         // if user doesn't already exist, add them
 		else
 		{
             // add user to DB
-            $stmt2 = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-            $stmt2->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
-            $stmt2->execute();
+            		$stmt2 = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+            		$stmt2->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
+            		$stmt2->execute();
 
-            // check for inserting error here?
 
-            $id = $conn->insert_id;
-            returnWithInfo( $inData["firstname"], $inData["lastname"], $id );
+            		// return user info
+            		$id = $conn->insert_id;
+            		returnWithInfo( $inData["firstname"], $inData["lastname"], $id );
 
-        }
+                        $stmt2->close();
+
+                }
 
 		$stmt1->close();
-        $stmt2->close();
 		$conn->close();
 	}
 	
